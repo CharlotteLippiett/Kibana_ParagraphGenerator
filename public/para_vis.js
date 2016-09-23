d7c66e50564701c4e20bc75e8bbfbbd77828de96
Charlotte Lippiett <charlotte248@btinternet.com>
2016-09-23 10:32:45 +0100

    initial commit

---
+++ public/para_vis.js
@@ -0,0 +1,46 @@
+define(function (require) {
+  // we need to load the css ourselves
+  require('plugins/para_vis/para_vis.less');
+
+  // we also need to load the controller and used by the template
+  require('plugins/para_vis/para_vis_controller');
+
+  // register the provider with the visTypes registry
+  require('ui/registry/vis_types').register(ParaVisProvider);
+
+  function ParaVisProvider(Private) {
+    const TemplateVisType = Private(require('ui/template_vis_type/TemplateVisType'));
+    const Schemas = Private(require('ui/Vis/Schemas'));
+
+    // return the visType object, which kibana will use to display and configure new
+    // Vis object of this type.
+    return new TemplateVisType({
+      name: 'para',
+      title: 'Paragraph Generator',
+      description: 'Create custom paragraphs/blocks of text with the addition of automatica paramters.',
+      icon: 'fa-calculator',
+      template: require('plugins/para_vis/para_vis.html'),
+      params: {
+        defaults: {
+          handleNoResults: true,
+          fontSize: 60
+        },
+        editor: require('plugins/para_vis/para_vis_params.html')
+      },
+      schemas: new Schemas([
+        {
+          group: 'paras',
+          name: 'para',
+          title: 'Paragraph Generator',
+          min: 1,
+          defaults: [
+            { type: 'count', schema: 'para' }
+          ]
+        }
+      ])
+    });
+  }
+
+  // export the provider so that the visType can be required with Private()
+  return ParaVisProvider;
+});
---
+++ public/para_vis_controller.js
@@ -0,0 +1,39 @@
+define(function (require) {
+  var _ = require('lodash');
+  // get the kibana/metric_vis module, and make sure that it requires the "kibana" module if it
+  // didn't already
+  const module = require('ui/modules').get('kibana/para_vis', ['kibana']);
+
+  module.controller('KbnParaVisController', function ($scope, Private) {
+    const tabifyAggResponse = Private(require('ui/agg_response/tabify/tabify'));
+
+    const paras = $scope.paras = [];
+
+    function isInvalid(val) {
+      return _.isUndefined(val) || _.isNull(val) || _.isNaN(val);
+    }
+
+    $scope.processTableGroups = function (tableGroups) {
+      tableGroups.tables.forEach(function (table) {
+        table.columns.forEach(function (column, i) {
+          const fieldFormatter = table.aggConfig(column).fieldFormatter();
+          let value = table.rows[0][i];
+
+          value = isInvalid(value) ? '?' : fieldFormatter(value);
+
+          paras.push({
+            label: column.title,
+            value: value
+          });
+        });
+      });
+    };
+
+    $scope.$watch('esResponse', function (resp) {
+      if (resp) {
+        paras.length = 0;
+        $scope.processTableGroups(tabifyAggResponse($scope.vis, resp));
+      }
+    });
+  });
+});
